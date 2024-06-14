75c6f1ebac553fa64419045ab2b563e53b0120c8
Максим Порошин <baiter159159@gmail.com>
2024-06-14 21:07:59 +0500

    flake8 brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
 import random
 from brain_games import cli
-def main():
 
+
+def answer(num):
+    if num % 2 == 0:
+        answer = "yes"
+    elif num % 2 != 0:
+        answer = "no"
+    return answer
+
+
+def main():
     print("Welcome to the Brain Games!")
     name = cli.welcome_user()
     print(f"Hello, {name}!")
     counter = 0
     print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
-    for i in range (1, 4):
+    for i in range(1, 4):
         num = random.randrange(1, 100)
         print(f"Question: {num}")
         input1 = input("Your answer: ")
-        if num % 2 == 0:
-            if input1 == "yes":
-                print("Correct!")
-                counter += 1
-            else:
-                print("'no' is wrong answer ;(. Correct answer was 'yes'.")
-                print(f"Let's try again, {name}!")
-                break  
-        if num % 2 != 0:
-            if input1 == "no":
-                print("Correct!")
-                counter += 1
-            else:
-                print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-                print(f"Let's try again, {name}!")
-                break 
+        if input1 == answer(num):
+            print("Correct!")
+            counter += 1
+        else:
+            print(f"{input1} is wrong answer ;(. ", end="")
+            print(f"Correct answer was {answer(num)}.")
+            print(f"Let's try again, {name}!")
+            break
     if counter == 3:
         print(f"Congratulations, {name}!")
+
 
 if __name__ == '__main__':
     main()
