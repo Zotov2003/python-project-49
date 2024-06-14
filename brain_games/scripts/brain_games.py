1062e2c7677083bbb3d3a701054ca0bb08c9aa37
Максим Порошин <baiter159159@gmail.com>
2024-06-14 21:11:36 +0500

    flake8 brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,11 +3,10 @@ from brain_games import cli
 
 
 def main():
-	print("Welcome to the Brain Games!")
-	name = cli.welcome_user()
-	print(f"Hello, {name}!")
+    print("Welcome to the Brain Games!")
+    name = cli.welcome_user()
+    print(f"Hello, {name}!")
+
 
 if __name__ == '__main__':
     main()
-
-
