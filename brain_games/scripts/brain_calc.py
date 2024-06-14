866c33fd5248c977244b79d9f3357ce4a2fed65b
Максим Порошин <baiter159159@gmail.com>
2024-06-14 20:51:10 +0500

    flake8 brain-calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,51 +1,42 @@
 #!/usr/bin/env python3
 import random
 from brain_games import cli
+
+
+def action1(x1, action, x2):
+    if action == "+":
+        result = x1 + x2
+    elif action == "-":
+        result = x1 - x2
+    elif action == "*":
+        result = x1 * x2
+    return str(result)
+
+
 def main():
     print("Welcome to the Brain Games!")
     name = cli.welcome_user()
     print(f"Hello, {name}!")
     print("What is the result of the expression?")
-    result = 0
     counter = 0
-
-    for i in range (1, 4):
-        
+    for i in range(1, 4):
         arithmetic = ["+", "-", "*"]
         x1 = random.randrange(1, 10)
         x2 = random.randrange(1, 10)
         action = random.choice(arithmetic)
         print(f"Question: {x1} {action} {x2}")
         input1 = input("Your answer: ")
-        if action == "+":
-            result = x1 + x2
-            if input1 == str(result):
-                counter += 1
-                print("Correct!")
-            else:
-                print(f"{input1} is  wrong answer ;(. Correct answer was {result}")
-                print(f"Let's try again, {name}!")
-                break
-        if action == "-":
-            result = x1 - x2
-            if input1 == str(result):
-                counter += 1
-                print("Correct!")
-            else:
-                print(f"{input1} is  wrong answer ;(. Correct answer was {result}")
-                print(f"Let's try again, {name}!")
-                break
-        if action == "*":
-            result = x1 * x2
-            if input1 == str(result):
-                counter += 1
-                print("Correct!")
-            else:
-                print(f"{input1} is  wrong answer ;(. Correct answer was {result}")
-                print(f"Let's try again, {name}!")
-                break
+        if input1 == action1(x1, action, x2):
+            counter += 1
+            print("Correct!")
+        else:
+            print(f"{input1} is  wrong answer ;(. ", end="")
+            print(f"Correct answer was {action1(x1, action, x2)}")
+            print(f"Let's try again, {name}!")
+            break
         if counter == 3:
             print(f"Congratulations, {name}!")
-            
+
+
 if __name__ == '__main__':
     main()
