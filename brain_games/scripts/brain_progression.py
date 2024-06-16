9bc3f938cc719fe649c6f6bddce0855c460c1634
Максим Порошин <baiter159159@gmail.com>
2024-06-16 12:39:40 +0500

    flake8 brain-progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 import random
 from brain_games import cli
+
+
 def main():
 
     print("Welcome to the Brain Games!")
     name = cli.welcome_user()
     print(f"Hello, {name}!")
-    counter = 0 
+    counter = 0
     print("What number is missing in the progression?")
-    
-    for i in range (1, 4):
+    for i in range(1, 4):
         start = random.randrange(1, 10)
         step = random.randrange(1, 5)
         index = random.randrange(1, 10)
@@ -17,17 +18,20 @@ def main():
         progression = [start + step * i for i in range(10)]
         num_res = progression[index]
         progression[index] = ".."
-        print("Question: ", end = "")
+        print("Question: ", end="")
         print(*progression)
         input1 = input("Your answer: ")
         if input1 == str(num_res):
             print("Correct!")
             counter += 1
         else:
-            print(f"'{input1}' is wrong answer ;(. Correct answer was '{num_res}'.\nLet's try again, {name}!")
+            print(f"'{input1}' is wrong answer ;(. ", end="")
+            print(f"Correct answer was '{num_res}'.")
+            print(f"Let's try again, {name}!")
             break
         if counter == 3:
             print(f"Congratulations, {name}!")
-            
+
+
 if __name__ == '__main__':
     main()
