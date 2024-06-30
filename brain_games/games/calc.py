d947fa79e820e6c891e01c683921ad4aca7f27aa
Максим Порошин <baiter159159@gmail.com>
2024-06-30 22:25:13 +0500

    fix brain-calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,12 +3,12 @@ import random
 CONDITION = "What is the result of the expression?"
 
 
-def action(x1, action, x2):
-    if action == "+":
+def action(x1, symbol, x2):
+    if symbol == "+":
         result = x1 + x2
-    elif action == "-":
+    elif symbol == "-":
         result = x1 - x2
-    elif action == "*":
+    elif symbol == "*":
         result = x1 * x2
     return str(result)
 
@@ -19,7 +19,7 @@ def game():
     arithmetic = ["+", "-", "*"]
     x1 = random.randrange(1, 10)
     x2 = random.randrange(1, 10)
-    action = random.choice(arithmetic)
-    operation = (f"{x1} {action} {x2}")
-    res = action(x1, action, x2)
+    symbol = random.choice(arithmetic)
+    operation = (f"{x1} {symbol} {x2}")
+    res = action(x1, symbol, x2)
     return operation, res
