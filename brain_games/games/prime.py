5e1b72b97f06148fe04e104adaccd160db0a78c1
Максим Порошин <baiter159159@gmail.com>
2024-07-03 21:01:48 +0500

    fix brain-prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,6 @@
 import random
 
-CONDITION = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
+CONDITION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
